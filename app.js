a4728bb3ba715c496f262d423a18bd11c525e382
ShrutikaLondhe15 <shrutikalondhe15@gmail.com>
2024-02-03 21:04:36 +0530

    Middleware

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const http = require("http")
 const express= require("express")
 const app=express()
 
 app.use((req,res,next) => {
-    console.log("In the middleware")
-    next()
+    console.log("In the middleware");
+    res.send("<h1>Hello from Middleware</h1>")
+    next();//allows the request to continue to the next middleware in line
 });
 
 app.use((req,res,next) => {
-    console.log("In second middleware")
+    console.log("In second middleware");
 });
 
-const server = http.createServer(app)
-server.listen(4000)
+app.listen(4000)
