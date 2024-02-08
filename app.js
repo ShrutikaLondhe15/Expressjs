9bdb8c75128fc069cbe706e9e2b55fc8429b9153
ShrutikaLondhe15 <shrutikalondhe15@gmail.com>
2024-02-08 21:02:24 +0530

    serving file statically

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express= require("express");
-const bodyParser=require("body-parser");
-const path = require('path')
+const express=require('express')
 const app=express();
+const path=require('path');
+const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 // const contactRoutes = require('/routes/contactus');
 
 app.use(bodyParser.urlencoded({extended : false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
