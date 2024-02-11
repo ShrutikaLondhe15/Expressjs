b24c0a2135ddef102a3b7f0f51b378083d1bc174
ShrutikaLondhe15 <shrutikalondhe15@gmail.com>
2024-02-11 20:36:46 +0530

    Controllers

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const express=require('express')
 const app=express();
 const path=require('path');
 const bodyParser = require('body-parser');
+const errorController = require('./controllers/error');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-// const contactRoutes = require('/routes/contactus');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
-// app.use(contactRoutes);
+app.use(errorController.get404);
 
-app.use((req,res,next) => {
-    res.status(404).sendFile(path.join(__dirname ,'views','404.html'));
-})
 
 app.listen(4000)
