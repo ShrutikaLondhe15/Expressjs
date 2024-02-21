d8452d61f2d33398bd77e29440c24107d68d4080
ShrutikaLondhe15 <shrutikalondhe15@gmail.com>
2024-02-21 20:19:04 +0530

    dynamic routing(t8)

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const express=require('express')
-const app=express();
-const path=require('path');
+const path = require('path');
+
+const express = require('express');
 const bodyParser = require('body-parser');
+
 const errorController = require('./controllers/error');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended : false}));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
 app.use(errorController.get404);
 
-
-app.listen(4000)
+app.listen(1000);
